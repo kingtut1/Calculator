15d5d96f000a8bdf058c8704ac78a702d1924408
Jordan Johnson <jjordan290@gmail.com>
2020-10-22 18:51:41 -0400

    It does something

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,26 @@
-def add(a, b):
-    return a + b
-def sub(a, b):
-    return a - b
+def add(a,b):
+    sum = a + b 
+    print("Sum: ", sum)
+
+
+def sub(a,b):
+    sum = a - b 
+    print("Difference:",sum) 
+
+
+def multi(a,b):
+    sum = a * b
+    print("Product:",sum)
+
+def quotient(a,b):
+    sum = a / b
+    print("Quotient:",sum)
 
 def main():
-    print( add(1, 2))
-    print("Hello")
-main()
+    a = int(input("Please enter a number: "))
+    b = int(input("Please enter another number: "))
+    
+    add(a,b)
+    sub(a,b)  
+    multi(a,b)
+    quotient(a,b)
